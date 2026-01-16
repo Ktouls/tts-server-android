e8e85309585ee55a4b2849cc376aec6ec7899881
Ktouls <75007654+Ktouls@users.noreply.github.com>
2026-01-17 03:50:15 +0800

    Update MsTtsForwarderConfig.kt

--- app/src/main/java/com/github/jing332/tts_server_android/conf/MsTtsForwarderConfig.kt
+++ app/src/main/java/com/github/jing332/tts_server_android/conf/MsTtsForwarderConfig.kt
@@ -1,27 +1,34 @@
 package com.github.jing332.tts_server_android.conf
 
+import android.content.Context
 import com.funny.data_saver.core.DataSaverPreferences
 import com.funny.data_saver.core.mutableDataSaverStateOf
 import com.github.jing332.tts_server_android.app
 
 object MsTtsForwarderConfig {
-    private val pref = DataSaverPreferences(app.getSharedPreferences("server", 0))
+    private val pref by lazy { DataSaverPreferences((app as Context).getSharedPreferences("server", 0)) }
 
-    val port = mutableDataSaverStateOf(
-        dataSaverInterface = pref,
-        key = "port",
-        initialValue = 1233
-    )
+    val port by lazy {
+        mutableDataSaverStateOf(
+            dataSaverInterface = pref,
+            key = "port",
+            initialValue = 1233
+        )
+    }
 
-    val isWakeLockEnabled = mutableDataSaverStateOf(
-        dataSaverInterface = pref,
-        key = "isWakeLockEnabled",
-        initialValue = false
-    )
+    val isWakeLockEnabled by lazy {
+        mutableDataSaverStateOf(
+            dataSaverInterface = pref,
+            key = "isWakeLockEnabled",
+            initialValue = false
+        )
+    }
 
-    val token = mutableDataSaverStateOf(
-        dataSaverInterface = pref,
-        key = "token",
-        initialValue = "",
-    )
+    val token by lazy {
+        mutableDataSaverStateOf(
+            dataSaverInterface = pref,
+            key = "token",
+            initialValue = "",
+        )
+    }
 }
