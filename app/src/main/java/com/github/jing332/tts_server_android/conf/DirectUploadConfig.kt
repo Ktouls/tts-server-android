ed37e3a10e3078db65d05d8d9db65e1820a52b39
Ktouls <75007654+Ktouls@users.noreply.github.com>
2026-01-17 03:49:48 +0800

    Update DirectUploadConfig.kt

--- app/src/main/java/com/github/jing332/tts_server_android/conf/DirectUploadConfig.kt
+++ app/src/main/java/com/github/jing332/tts_server_android/conf/DirectUploadConfig.kt
@@ -1,16 +1,19 @@
 package com.github.jing332.tts_server_android.conf
 
+import android.content.Context
 import com.funny.data_saver.core.DataSaverPreferences
 import com.funny.data_saver.core.mutableDataSaverStateOf
 import com.github.jing332.tts_server_android.app
 import com.github.jing332.common.utils.FileUtils.readAllText
 
 object DirectUploadConfig {
-    private val pref = DataSaverPreferences(app.getSharedPreferences("direct_link_upload", 0))
+    private val pref by lazy { DataSaverPreferences((app as Context).getSharedPreferences("direct_link_upload", 0)) }
 
-    val code = mutableDataSaverStateOf(
-        pref,
-        key = "code",
-        app.assets.open("defaultData/direct_link_upload.js").readAllText()
-    )
+    val code by lazy {
+        mutableDataSaverStateOf(
+            pref,
+            key = "code",
+            initialValue = (app as Context).assets.open("defaultData/direct_link_upload.js").readAllText()
+        )
+    }
 }
