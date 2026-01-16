2f52faa3ea00e13e3f04a97b77815769b53686fd
Ktouls <75007654+Ktouls@users.noreply.github.com>
2026-01-17 03:50:45 +0800

    Update PluginConfig.kt

--- app/src/main/java/com/github/jing332/tts_server_android/conf/PluginConfig.kt
+++ app/src/main/java/com/github/jing332/tts_server_android/conf/PluginConfig.kt
@@ -1,12 +1,12 @@
 package com.github.jing332.tts_server_android.conf
 
+import android.content.Context
 import com.funny.data_saver.core.DataSaverPreferences
 import com.funny.data_saver.core.mutableDataSaverStateOf
 import com.github.jing332.tts_server_android.app
 
 object PluginConfig {
-    private val pref = DataSaverPreferences(app.getSharedPreferences("plugin", 0))
+    private val pref by lazy { DataSaverPreferences((app as Context).getSharedPreferences("plugin", 0)) }
 
-    val textParam = mutableDataSaverStateOf(pref, key = "sampleText", "示例文本。 Sample text.")
-//    val isSaveRhinoLog = mutableDataSaverStateOf(pref, key = "isSaveRhinoLog", false)
+    val textParam by lazy { mutableDataSaverStateOf(pref, key = "sampleText", "示例文本。 Sample text.") }
 }
