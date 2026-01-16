6893aa782b48e217b8988eda20930a06bd104080
Ktouls <75007654+Ktouls@users.noreply.github.com>
2026-01-17 04:10:36 +0800

    Update ReplaceRuleManagerViewModel.kt

--- app/src/main/java/com/github/jing332/tts_server_android/compose/systts/replace/ReplaceRuleManagerViewModel.kt
+++ app/src/main/java/com/github/jing332/tts_server_android/compose/systts/replace/ReplaceRuleManagerViewModel.kt
@@ -1,5 +1,6 @@
 package com.github.jing332.tts_server_android.compose.systts.replace
 
+import android.content.Context
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -15,16 +16,12 @@ import com.github.jing332.database.entities.replace.ReplaceRuleGroup
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.conflate
 import kotlinx.coroutines.launch
 
 internal class ReplaceRuleManagerViewModel : ViewModel() {
     private var allList = listOf<GroupWithReplaceRule>()
-
     private val _list = MutableStateFlow<List<GroupWithReplaceRule>>(emptyList())
-    val list: MutableStateFlow<List<GroupWithReplaceRule>>
-        get() = _list
-
+    val list: MutableStateFlow<List<GroupWithReplaceRule>> get() = _list
     var searchType by mutableStateOf(SearchType.NAME)
     var searchText by mutableStateOf("")
 
@@ -34,11 +31,10 @@ internal class ReplaceRuleManagerViewModel : ViewModel() {
                 dbm.replaceRuleDao.insertGroup(
                     ReplaceRuleGroup(
                         DEFAULT_GROUP_ID,
-                        app.getString(R.string.default_group)
+                        (app as Context).getString(R.string.default_group)
                     )
                 )
             }
-
             dbm.replaceRuleDao.updateAllOrder()
             dbm.replaceRuleDao.flowAllGroupWithReplaceRules().collectLatest {
                 allList = it
@@ -46,7 +42,6 @@ internal class ReplaceRuleManagerViewModel : ViewModel() {
             }
         }
     }
-
 
     fun updateSearchResult(
         text: String = searchText,
@@ -57,52 +52,27 @@ internal class ReplaceRuleManagerViewModel : ViewModel() {
             _list.value = src
             return
         }
-
         val resultList = mutableListOf<GroupWithReplaceRule>()
         src.forEach {
             val subList = mutableListOf<ReplaceRule>()
             val groupWithRules = GroupWithReplaceRule(it.group, subList)
             resultList.add(groupWithRules)
-
             it.list.forEach { rule ->
                 when (type) {
-                    SearchType.GROUP_NAME -> {
-                        if (it.group.name.contains(text)) subList.add(rule)
-                    }
-
-                    SearchType.NAME -> {
-                        if (rule.name.contains(text)) subList.add(rule)
-                    }
-
-                    SearchType.PATTERN -> {
-                        if (rule.pattern.contains(text)) subList.add(rule)
-                    }
-
-                    SearchType.REPLACEMENT -> {
-                        if (rule.replacement.contains(text)) subList.add(rule)
-                    }
+                    SearchType.GROUP_NAME -> if (it.group.name.contains(text)) subList.add(rule)
+                    SearchType.NAME -> if (rule.name.contains(text)) subList.add(rule)
+                    SearchType.PATTERN -> if (rule.pattern.contains(text)) subList.add(rule)
+                    SearchType.REPLACEMENT -> if (rule.replacement.contains(text)) subList.add(rule)
                 }
             }
-
-            if (subList.isEmpty())
-                resultList.remove(groupWithRules)
+            if (subList.isEmpty()) resultList.remove(groupWithRules)
         }
-
         _list.value = resultList
     }
 
-    fun moveTop(rule: ReplaceRule) {
-        dbm.replaceRuleDao.update(rule.copy(order = 0))
-    }
-
-    fun moveBottom(rule: ReplaceRule) {
-        dbm.replaceRuleDao.update(rule.copy(order = dbm.replaceRuleDao.count))
-    }
-
-    fun deleteRule(rule: ReplaceRule) {
-        dbm.replaceRuleDao.delete(rule)
-    }
-
+    fun moveTop(rule: ReplaceRule) { dbm.replaceRuleDao.update(rule.copy(order = 0)) }
+    fun moveBottom(rule: ReplaceRule) { dbm.replaceRuleDao.update(rule.copy(order = dbm.replaceRuleDao.count)) }
+    fun deleteRule(rule: ReplaceRule) { dbm.replaceRuleDao.delete(rule) }
     fun deleteGroup(groupWithRules: GroupWithReplaceRule) {
         dbm.replaceRuleDao.delete(*groupWithRules.list.toTypedArray())
         dbm.replaceRuleDao.deleteGroup(groupWithRules.group)
