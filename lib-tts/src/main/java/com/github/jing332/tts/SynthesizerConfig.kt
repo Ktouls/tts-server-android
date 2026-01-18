e60f5b0b8de08f8b4d6236bc4cd65f2e6690d7d5
Ktouls <75007654+Ktouls@users.noreply.github.com>
2026-01-18 10:13:35 +0800

    Update SynthesizerConfig.kt

--- lib-tts/src/main/java/com/github/jing332/tts/SynthesizerConfig.kt
+++ lib-tts/src/main/java/com/github/jing332/tts/SynthesizerConfig.kt
@@ -5,7 +5,9 @@ import com.github.jing332.database.entities.systts.AudioParams
 typealias ValueProvider<T> = () -> T
 
 data class SynthesizerConfig(
-    var requestTimeout: ValueProvider<Long> = { 8000 },
+    // 【修改点】默认超时从 8000 改为 300000 (5分钟)
+    // 确保全局单例在未被 UI 配置覆盖时，也有足够的配额让插件重试
+    var requestTimeout: ValueProvider<Long> = { 300000 },
     var maxRetryTimes: ValueProvider<Int> = { 1 },
     var toggleTry: ValueProvider<Int> = { 1 },
     var streamPlayEnabled: ValueProvider<Boolean> = { true },
